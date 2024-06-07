5e84937750f7c86e00c531fdad5aac58db373c92
TobyRoberts1 <163478842+TobyRoberts1@users.noreply.github.com>
2024-06-07 20:39:28 +1200

    5.1 working however 5.2 is off, possibly rounding

--- assignment4.py
+++ assignment4.py
@@ -67,46 +67,44 @@ def query_n_gram(model, sequence):
 
 
 def blend_predictions(preds, factor=0.8):
-    # Task 3
-    # Return a blended prediction as a dictionary.
-    # Replace the line below with your code.
-
     from collections import Counter
 
-    #remove NONE values from preds
+    # Remove NONE values from preds
     preds = [p for p in preds if p is not None]
 
-    #normilize each pred to sum to 1 
-    norm_preds = []
+    # Normalize each pred to sum to 1 
     for pred in preds: 
-        factor = 1.0/sum( pred.values())
-        for k in pred:
-            pred[k] = pred[k]*factor 
+        total = sum(pred.values())
+        if total > 0:
+            normalization_factor = 1.0 / total
+            for k in pred:
+                pred[k] *= normalization_factor 
 
-        #if total is larger then 1 normilze 
-        
-    #Blend according to the factor 
-    #init blended and weights 
+    # Initialize blended predictions and remaining weight
     blended = {}
-    col_list = []
+    remaining_weight = 1.0
 
-
-    #loop through each pred and calc weight
+    # Loop through each normalized pred and calculate weight
     for i in range(len(preds)):
-        if i == 0: 
-            weight = 0.8
-        else: 
-            weight = math.pow((1.0 - 0.8),(i))
-        
+        if i == len(preds) - 1:
+            # Last prediction takes all remaining weight
+            weight = remaining_weight
+        else:
+            weight = factor * remaining_weight
+            remaining_weight -= weight
+
         for token in preds[i]:
-            preds[i][token] *= weight 
+            if token not in blended:
+                blended[token] = 0
+            blended[token] += preds[i][token] * weight
 
+    # Normalize blended predictions to sum to 1
+    total_blended = sum(blended.values())
+    if total_blended > 0:
+        blended = {k: v / total_blended for k, v in blended.items()}
 
-    # add to the list 
-    for i in range(len(preds)):
-        col_list.append(Counter(preds[i]))
-    blended = dict(sum(col_list, Counter()))
     return blended
+
 
 
 
@@ -129,14 +127,6 @@ def predict(sequence, models):
     blend_pred= blend_predictions(all_pred)
     preds = random.choices(list(blend_pred.keys()), weights=[blend_pred[i] for i in blend_pred], k =1 )[0]
     return preds
-        
-
-
-    
-
-    
-
-
 
 
 def log_likelihood_ramp_up(sequence, models):
@@ -152,47 +142,84 @@ def log_likelihood_ramp_up(sequence, models):
         if i == 0:
             context = ()
         else: 
-            #gets correct length of n gram for other tokens
+            #gets correct length of n gram for the rest that do have context 
             context = tuple(sequence[max(0, i - len_models):i])
         model_index = min(len(context), len_models)
    
-        #model = models[model_index]
-        model = {():models[model_index]}
-      
-        #print(models)
-        
-        
-        #query model with the correct context 
-  
+        model = models[len_models -model_index]
+   
+        #query model with the correct context  
         pred = query_n_gram(model, context)
         
-        #print(f"Model: {model}")
-        # print(f"token: {token}")
-        # print(f" contexst: {context}")
-        # print(f"model index: {model_index}")
-        # print(f"pred {pred}")
-
+        
         #return -math.inf if doesn't exist 
         if pred is None or token not in pred:
-            print("didn't get through")
             return -math.inf
         
         prob = pred[token] / sum(pred.values())
        
 
         log_likelihood += math.log(prob)
-  
+    #i beleive in this it is slightly rounded or possible just wrong as it returns about 0.0000000000000056 smaller then meant to be. 
+    #I think this is from in task 3 instead of summing to 1 it sums to 1.00000000000000002
     return log_likelihood
 
 
 
-
 def log_likelihood_blended(sequence, models):
-
     # Task 5.2
     # Return a log likelihood value of the sequence based on the models.
     # Replace the line below with your code.
-    raise NotImplementedError
+    blended_log_likelihood = 0 
+    len_models = len(models) - 1
+    model = {}
+    for i in range(len(sequence)):
+        token = sequence[i]
+        #for unigrams the first loop through should have no context 
+        if i == 0:
+            context = ()
+        else: 
+            #gets correct length of n gram for the rest that do have context 
+            context = tuple(sequence[max(0, i - len_models):i])
+
+        all_preds = []
+        for model in models:
+            #query model with the correct context  
+            pred = query_n_gram(model, context)
+            all_preds.append(pred)
+        
+        blended_pred = blend_predictions(all_preds)
+        
+
+        #return -math.inf if doesn't exist  
+        if pred is None or token not in pred:
+            return -math.inf
+
+        # calculates the probability of the token
+        prob = blended_pred[token]
+        #add the log of prob to get the log_likelihood 
+        blended_log_likelihood += math.log(prob)
+  
+    return blended_log_likelihood
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -244,7 +271,6 @@ if __name__ == '__main__':
     
 
     # Task 5.2 test code
-    '''
+    
     print(log_likelihood_blended(sequence[:20], models))
-    '''
-
+    
